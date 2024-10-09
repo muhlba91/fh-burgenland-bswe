1f8661cda14f4dcbe129c8791b61e68e054ba9e2
Marcel T.O <marcel@turobin-ort.at>
2024-10-09 19:03:47 +0200

    docs: Remove unnecessary comment in StringController

--- src/main/java/io/muehlbachler/bswe/StringController.java
+++ src/main/java/io/muehlbachler/bswe/StringController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class StringController {
     /**
      * Represents the service provided for this controller
-     * 
      * @see StringService
      */
     @Autowired
