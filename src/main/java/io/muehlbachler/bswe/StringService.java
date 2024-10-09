86a03e068ea12981c89e4069e9a69a0762969f2f
manuel <mn07@gmx.at>
2024-10-09 19:09:33 +0200

    docs: add javadoc to StringService.java

--- src/main/java/io/muehlbachler/bswe/StringService.java
+++ src/main/java/io/muehlbachler/bswe/StringService.java
@@ -2,12 +2,21 @@ package io.muehlbachler.bswe;
 
 import org.springframework.stereotype.Service;
 
-// FIXME: add javadoc
+/**
+ * Service class for handling string manipulations.
+ */
 @Service
 public class StringService {
-    // FIXME: add javadoc
+    /**
+     * Converts the given string to uppercase.
+     *
+     * @param string the input string to be converted
+     * @return the uppercase version of the input string, or null if input is null
+     */
     public String toUpperCase(final String string) {
+
         // FIXME: implement
+
         return string;
     }
 }
